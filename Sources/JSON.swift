1ff305c6fa75cdc7fd2c6e0fe91e4c90d4d38dcf
Marko Tadic <tadija@me.com>
2017-02-17 19:11:16 +0100

    Initial commit

---
+++ Sources/JSON.swift
@@ -0,0 +1,4 @@
+struct JSON {
+
+    var text = "Hello, World!"
+}
---
+++ Tests/JSONTests/JSONTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import JSON
+
+class JSONTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(JSON().text, "Hello, World!")
+    }
+
+
+    static var allTests : [(String, (JSONTests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
