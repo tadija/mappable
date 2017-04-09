f127ac177ffefeb4e43346a53cd943827178d6a9
Marko Tadic <tadija@me.com>
2017-04-09 15:48:42 +0200

    Created initial structure for new cocoa touch framework project

---
+++ Mappable/Sources/Mappable.h
@@ -0,0 +1,19 @@
+//
+//  Mappable.h
+//  Mappable
+//
+//  Created by Marko Tadić on 4/9/17.
+//  Copyright © 2017 AE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mappable.
+FOUNDATION_EXPORT double MappableVersionNumber;
+
+//! Project version string for Mappable.
+FOUNDATION_EXPORT const unsigned char MappableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mappable/PublicHeader.h>
+
+
